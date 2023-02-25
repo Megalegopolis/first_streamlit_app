4e74ca8cd8ff845ecf7c27ecc97f3520f9245750
Megalegopolis <113646195+Megalegopolis@users.noreply.github.com>
2023-02-25 04:11:29 +0100

    else if function

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,15 +27,22 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice) 
+  #take the json version of the response and normalize it
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+#streamlit.write('The user entered ', fruit_choice)
+
 # streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 
-#take the json version of the response and normalize it
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+
 #output it the screeen as table
-streamlit.dataframe(fruityvice_normalized)
+
 
 # do not run anything past here while troubleshoot
 streamlit.stop()
